793d26d3f5e0e28d66b8395ad70d59515709809a
Misha Samoylov <inoveral@tutanota.com>
2021-04-26 23:57:54 +0700

    Change windows variable to static

--- src/window.c
+++ src/window.c
@@ -1,6 +1,6 @@
 #include "window.h"
 
-GLFWwindow* window;
+static GLFWwindow* window;
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
